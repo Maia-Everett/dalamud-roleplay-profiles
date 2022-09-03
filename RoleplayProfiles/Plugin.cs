d3f4616de89abde99172680313b1e0732dc4aae5
Maia Everett <maia@everett.one>
2022-09-03 21:38:52 +0700

    Remove slash command (unnecessary)

--- RoleplayProfiles/Plugin.cs
+++ RoleplayProfiles/Plugin.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Reflection;
 using Dalamud.Interface.Windowing;
 using SamplePlugin.Windows;
+using Dalamud.Game.ClientState.Objects;
 
 namespace SamplePlugin
 {
@@ -13,8 +14,6 @@ namespace SamplePlugin
         public string Name => "Roleplay Profiles";
         private const string CommandName = "/rpp";
 
-        private DalamudPluginInterface PluginInterface { get; init; }
-        private CommandManager CommandManager { get; init; }
         public Configuration Configuration { get; init; }
         public WindowSystem WindowSystem = new("RoleplayProfiles");
 
@@ -23,13 +22,10 @@ namespace SamplePlugin
 
         public Plugin(
             [RequiredVersion("1.0")] DalamudPluginInterface pluginInterface,
-            [RequiredVersion("1.0")] CommandManager commandManager)
+            [RequiredVersion("1.0")] TargetManager targetManager)
         {
-            this.PluginInterface = pluginInterface;
-            this.CommandManager = commandManager;
-
-            this.Configuration = this.PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
-            this.Configuration.Initialize(this.PluginInterface);
+            this.Configuration = pluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
+            this.Configuration.Initialize(pluginInterface);
 
             configWindow = new ConfigWindow(this);
             WindowSystem.AddWindow(configWindow);
@@ -37,24 +33,13 @@ namespace SamplePlugin
             tooltipWindow = new TooltipWindow(this);
             WindowSystem.AddWindow(tooltipWindow);
 
-            this.CommandManager.AddHandler(CommandName, new CommandInfo(OnCommand)
-            {
-                HelpMessage = "A useful message to display in /xlhelp"
-            });
-
-            this.PluginInterface.UiBuilder.Draw += DrawUI;
-            this.PluginInterface.UiBuilder.OpenConfigUi += DrawConfigUI;
+            pluginInterface.UiBuilder.Draw += DrawUI;
+            pluginInterface.UiBuilder.OpenConfigUi += DrawConfigUI;
         }
 
         public void Dispose()
         {
             this.WindowSystem.RemoveAllWindows();
-            this.CommandManager.RemoveHandler(CommandName);
-        }
-
-        private void OnCommand(string command, string args)
-        {
-            tooltipWindow.IsOpen = true;
         }
 
         private void DrawUI()
